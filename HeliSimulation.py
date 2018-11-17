7e287377a2603a5fdd27e2bc7555107fe96883a1
cbilsing <clemensbil@web.de>
2018-11-17 15:15:52 +0100

    first commit

--- HeliSimulation.py
+++ HeliSimulation.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.integrate
 import ModelConstants as mc
 from enum import Enum
+import time
 
 
 class ModelType(Enum):
@@ -45,18 +46,126 @@ class StateLimits(object):
         self.eps_e = 0
         self.eps_lamb = 0
 
+def event_pmin(t, x):
+    return x[0] - StateLimits.p_min
+event_pmin.terminal = True
+
+def event_pmax(t, x):
+    return x[0] - StateLimits.p_max
+event_pmax.terminal = True
+
+def event_pdt0(t, x):
+    return x[3]
+event_pdt0.terminal = True
+
+def event_emin(t, x):
+    return x[1] - StateLimits.e_min
+event_emin.terminal = True
+
+def event_emax(t, x):
+    return x[1] - StateLimits.e_max
+event_emax.terminal = True
+
+def event_edt0(t, x):
+    return x[4]
+event_edt0.terminal = True
+
 
 class HeliSimulation(object):
     def __init__(self, theta1_0, theta2_0, theta3_0, timeStep):
         """Initializes the simulation"""
         self.currentState = np.array([theta1_0, theta2_0, theta3_0, 0, 0, 0])
         self.should_check_limits = True
-        self.limitedStates = StateLimits()
+        self.statLim = StateLimits()
         self.timeStep = timeStep
         self.model_type = ModelType.CENTRIPETAL
-        self.solver = scipy.integrate.ode(lambda t, x: self.rhs(t, x, self.V_s, self.V_d))
-        self.solver.set_initial_value([theta1_0, theta2_0, theta3_0, 0, 0, 0])
-        self.solver.set_integrator('vode', method='adams', rtol=1e-6, atol=1e-9)
+        self.currentTime = 0
+
+    # def checkLimits(self):
+    #     """handles the limit state machine"""
+    #     p, e, lamb, dp, de, dlamb = self.currentState
+    #
+    #     #pitch limits
+    #     if self.limitedStates.p == LimitType.NO_LIMIT_REACHED:
+    #         if self.currentState[0] > self.limitedStates.p_max - self.limitedStates.eps_p:
+    #             print("[DEBUG] Reached upper limit of p")
+    #             self.limitedStates.p = LimitType.UPPER_LIMIT
+    #             p = self.limitedStates.p_max
+    #             dp = 0
+    #         if self.currentState[0] < self.limitedStates.p_min + self.limitedStates.eps_p:
+    #             print("[DEBUG] Reached lower limit of p")
+    #             self.limitedStates.p = LimitType.LOWER_LIMIT
+    #             p = self.limitedStates.p_min
+    #             dp = 0
+    #     if self.limitedStates.p == LimitType.UPPER_LIMIT:
+    #         if p + dp * self.timeStep < self.limitedStates.p_max - self.limitedStates.eps_p:
+    #             print("[DEBUG] p is not limited anymore")
+    #             self.limitedStates.p = LimitType.NO_LIMIT_REACHED
+    #         else:
+    #             p = self.limitedStates.p_max
+    #             dp = 0
+    #     if self.limitedStates.p == LimitType.LOWER_LIMIT:
+    #         if p + dp * self.timeStep > self.limitedStates.p_min + self.limitedStates.eps_p:
+    #             print("[DEBUG] p is not limited anymore")
+    #             self.limitedStates.p = LimitType.NO_LIMIT_REACHED
+    #         else:
+    #             p = self.limitedStates.p_min
+    #             dp = 0
+    #     #elevation limits
+    #     if self.limitedStates.e == LimitType.NO_LIMIT_REACHED:
+    #         if self.currentState[1] > self.limitedStates.e_max - self.limitedStates.eps_e:
+    #             print("[DEBUG] Reached upper limit of e")
+    #             self.limitedStates.e = LimitType.UPPER_LIMIT
+    #             e = self.limitedStates.e_max
+    #             de =  0
+    #         if self.currentState[1] < self.limitedStates.e_min + self.limitedStates.eps_e:
+    #             print("[DEBUG] Reached lower limit of e")
+    #             self.limitedStates.e = LimitType.LOWER_LIMIT
+    #             e = self.limitedStates.e_min
+    #             de = 0
+    #     if self.limitedStates.e == LimitType.UPPER_LIMIT:
+    #         if e + de * self.timeStep < self.limitedStates.e_max - self.limitedStates.eps_e:
+    #             print("[DEBUG] e is not limited anymore")
+    #             self.limitedStates.e = LimitType.NO_LIMIT_REACHED
+    #         else:
+    #             e = self.limitedStates.e_max
+    #             de = 0
+    #     if self.limitedStates.e == LimitType.LOWER_LIMIT:
+    #         if e + de * self.timeStep > self.limitedStates.e_min + self.limitedStates.eps_e:
+    #             print("[DEBUG] e is not limited anymore")
+    #             self.limitedStates.e = LimitType.NO_LIMIT_REACHED
+    #         else:
+    #             e = self.limitedStates.e_min
+    #             de = 0
+    #     #travel angle limits
+    #     if self.limitedStates.lamb == LimitType.NO_LIMIT_REACHED:
+    #         if self.currentState[2] > self.limitedStates.lamb_max - self.limitedStates.eps_lamb:
+    #             print("[DEBUG] Reached upper limit of lambda")
+    #             self.limitedStates.lamb = LimitType.UPPER_LIMIT
+    #             lamb = self.limitedStates.lamb_max
+    #             dlamb = 0
+    #         if self.currentState[2] < self.limitedStates.lamb_min + self.limitedStates.eps_lamb:
+    #             print("[DEBUG] Reached lower limit of lambda")
+    #             self.limitedStates.lamb = LimitType.LOWER_LIMIT
+    #             lamb = self.limitedStates.lamb_min
+    #             dlamb = 0
+    #     if self.limitedStates.lamb == LimitType.UPPER_LIMIT:
+    #         if lamb + dlamb * self.timeStep < self.limitedStates.lamb_max - self.limitedStates.eps_lamb:
+    #             print("[DEBUG] lambda is not limited anymore")
+    #             self.limitedStates.e = LimitType.NO_LIMIT_REACHED
+    #         else:
+    #             lamb = self.limitedStates.lamb_max
+    #             dlamb = 0
+    #     if self.limitedStates.lamb == LimitType.LOWER_LIMIT:
+    #         if lamb + dlamb * self.timeStep > self.limitedStates.lamb_min + self.limitedStates.eps_lamb:
+    #             print("[DEBUG] lambda is not limited anymore")
+    #             self.limitedStates.lamb = LimitType.NO_LIMIT_REACHED
+    #         else:
+    #             lamb = self.limitedStates.lamb_min
+    #             dlamb = 0
+    #
+    #     self.solver.set_initial_value([p, e, lamb, dp, de, dlamb], self.solver.t)
+    #     return
 
     def rhs(self, t, x, V_s, V_d):
         """Right hand side of the differential equation being integrated"""
@@ -78,117 +187,45 @@ class HeliSimulation(object):
             dde = (L_2/J_e) * np.cos(e) + (L_3/J_e) * np.cos(p) * V_s - (mc.d_e / J_e) * de - np.cos(e) * np.sin(e) * dlamb**2
             ddlamb = (L_4/J_l) * np.cos(e) * np.sin(p) * V_s - (mc.d_l / J_l) * dlamb
 
-        return [dp, de, dlamb, ddp, dde, ddlamb]
-
-    def checkLimits(self):
-        """handles the limit state machine"""
-        p, e, lamb, dp, de, dlamb = self.currentState
-
-        #pitch limits
-        if self.limitedStates.p == LimitType.NO_LIMIT_REACHED:
-            if self.currentState[0] > self.limitedStates.p_max - self.limitedStates.eps_p:
-                print("[DEBUG] Reached upper limit of p")
-                self.limitedStates.p = LimitType.UPPER_LIMIT
-                p = self.limitedStates.p_max
-                dp = 0
-            if self.currentState[0] < self.limitedStates.p_min + self.limitedStates.eps_p:
-                print("[DEBUG] Reached lower limit of p")
-                self.limitedStates.p = LimitType.LOWER_LIMIT
-                p = self.limitedStates.p_min
-                dp = 0
-        if self.limitedStates.p == LimitType.UPPER_LIMIT:
-            if p + dp * self.timeStep < self.limitedStates.p_max - self.limitedStates.eps_p:
-                print("[DEBUG] p is not limited anymore")
-                self.limitedStates.p = LimitType.NO_LIMIT_REACHED
-            else:
-                p = self.limitedStates.p_max
-                dp = 0
-        if self.limitedStates.p == LimitType.LOWER_LIMIT:
-            if p + dp * self.timeStep > self.limitedStates.p_min + self.limitedStates.eps_p:
-                print("[DEBUG] p is not limited anymore")
-                self.limitedStates.p = LimitType.NO_LIMIT_REACHED
-            else:
-                p = self.limitedStates.p_min
-                dp = 0
-        #elevation limits
-        if self.limitedStates.e == LimitType.NO_LIMIT_REACHED:
-            if self.currentState[1] > self.limitedStates.e_max - self.limitedStates.eps_e:
-                print("[DEBUG] Reached upper limit of e")
-                self.limitedStates.e = LimitType.UPPER_LIMIT
-                e = self.limitedStates.e_max
-                de =  0
-            if self.currentState[1] < self.limitedStates.e_min + self.limitedStates.eps_e:
-                print("[DEBUG] Reached lower limit of e")
-                self.limitedStates.e = LimitType.LOWER_LIMIT
-                e = self.limitedStates.e_min
-                de = 0
-        if self.limitedStates.e == LimitType.UPPER_LIMIT:
-            if e + de * self.timeStep < self.limitedStates.e_max - self.limitedStates.eps_e:
-                print("[DEBUG] e is not limited anymore")
-                self.limitedStates.e = LimitType.NO_LIMIT_REACHED
-            else:
-                e = self.limitedStates.e_max
-                de = 0
-        if self.limitedStates.e == LimitType.LOWER_LIMIT:
-            if e + de * self.timeStep > self.limitedStates.e_min + self.limitedStates.eps_e:
-                print("[DEBUG] e is not limited anymore")
-                self.limitedStates.e = LimitType.NO_LIMIT_REACHED
-            else:
-                e = self.limitedStates.e_min
-                de = 0
-        #travel angle limits
-        if self.limitedStates.lamb == LimitType.NO_LIMIT_REACHED:
-            if self.currentState[2] > self.limitedStates.lamb_max - self.limitedStates.eps_lamb:
-                print("[DEBUG] Reached upper limit of lambda")
-                self.limitedStates.lamb = LimitType.UPPER_LIMIT
-                lamb = self.limitedStates.lamb_max
-                dlamb = 0
-            if self.currentState[2] < self.limitedStates.lamb_min + self.limitedStates.eps_lamb:
-                print("[DEBUG] Reached lower limit of lambda")
-                self.limitedStates.lamb = LimitType.LOWER_LIMIT
-                lamb = self.limitedStates.lamb_min
-                dlamb = 0
-        if self.limitedStates.lamb == LimitType.UPPER_LIMIT:
-            if lamb + dlamb * self.timeStep < self.limitedStates.lamb_max - self.limitedStates.eps_lamb:
-                print("[DEBUG] lambda is not limited anymore")
-                self.limitedStates.e = LimitType.NO_LIMIT_REACHED
-            else:
-                lamb = self.limitedStates.lamb_max
-                dlamb = 0
-        if self.limitedStates.lamb == LimitType.LOWER_LIMIT:
-            if lamb + dlamb * self.timeStep > self.limitedStates.lamb_min + self.limitedStates.eps_lamb:
-                print("[DEBUG] lambda is not limited anymore")
-                self.limitedStates.lamb = LimitType.NO_LIMIT_REACHED
-            else:
-                lamb = self.limitedStates.lamb_min
-                dlamb = 0
-
-        self.solver.set_initial_value([p, e, lamb, dp, de, dlamb], self.solver.t)
-        return
+        return np.array([dp, de, dlamb, ddp, dde, ddlamb])
 
     def calcStep(self, V_f, V_b):
         """Returns the state of the system after the next time step
         V_f: voltage of the propeller right at back (of Fig.7) / first endeffector
         V_b: voltage of the propeller right at front (of Fig. 7) / second endeffector"""
-        self.V_s = V_f + V_b
-        self.V_d = V_f - V_b
+        #start = time.time()
+        V_s = V_f + V_b
+        V_d = V_f - V_b
         # checkLimits is called before integrate because the solver calls the rhs function several times
         # between two discrete steps. if there are state transitions between two steps, the solver
         # will probably react unpredictable
-        if self.should_check_limits:
+        if self.should_check_limits and 1 == 2:
             self.checkLimits()
-        self.currentState = self.solver.integrate(self.solver.t + self.timeStep)
+        #integrate one time step
+        #for adjusting the performance the number of in-between-steps can be changed
+        tt = np.linspace(self.currentTime, self.currentTime + self.timeStep, 3)
+        sol = scipy.integrate.solve_ivp(lambda t, x: self.rhs(t, x, V_s, V_d),
+                                        (self.currentTime, self.currentTime+self.timeStep), self.currentState,
+                                        method='RK45', t_eval=tt,  rtol=1e-6, atol=1e-9,
+                                        events=[event_pmin, event_pmax, event_pdt0,
+                                                event_emin, event_emax, event_edt0])
+        #check if there was an event in order to limit the angle states
+        if np.size(sol.t_events) != 0:
+            print(sol.t_events)
+        self.currentState = sol.y[:, -1]
+        self.currentTime = sol.t[-1]
+        #end = time.time()
+        #print(end - start)
         return self.currentState
 
     def getCurrentState(self):
         return self.currentState #yes, this is technically seen unnecessary, but accessing 'private' member objects from outside is rather ugly
 
     def getCurrentTime(self):
-        return self.solver.t
+        return self.currentTime
 
     def setCurrentStateAndTime(self, state, time=0.0):
         self.currentState = np.array(state)
-        self.solver.set_initial_value(self.currentState, time)
 
     def setModelType(self, modelType):
         self.model_type = modelType
