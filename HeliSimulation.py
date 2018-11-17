af9d2ba97d0f179127dfabbbb22695d271f73fac
cbilsing <clemensbil@web.de>
2018-11-17 16:13:53 +0100

    second commit

--- HeliSimulation.py
+++ HeliSimulation.py
@@ -32,19 +32,20 @@ class StateLimits(object):
     Helper class just for saving which states have reached their limit at the current time step,
     but also for saving some attributes about the limits
     """
+    p_max = 80 / 180.0 * np.pi
+    p_min = -p_max
+    e_max = 70.0 / 180.0 * np.pi
+    e_min = -e_max
+    lamb_max = 170.0 / 180.0 * np.pi
+    lamb_min = -lamb_max
+    eps_p = 0
+    eps_e = 0
+    eps_lamb = 0
+
     def __init__(self):
         self.p = LimitType.NO_LIMIT_REACHED
         self.e = LimitType.NO_LIMIT_REACHED
         self.lamb = LimitType.NO_LIMIT_REACHED
-        self.p_max = 80 / 180.0 * np.pi
-        self.p_min = -self.p_max
-        self.e_max = 70.0 / 180.0 * np.pi
-        self.e_min = -self.e_max
-        self.lamb_max = 170.0 / 180.0 * np.pi
-        self.lamb_min = -self.lamb_max
-        self.eps_p = 0
-        self.eps_e = 0
-        self.eps_lamb = 0
 
 def event_pmin(t, x):
     return x[0] - StateLimits.p_min
@@ -55,7 +56,7 @@ def event_pmax(t, x):
 event_pmax.terminal = True
 
 def event_pdt0(t, x):
-    return x[3]
+    return x[6]
 event_pdt0.terminal = True
 
 def event_emin(t, x):
@@ -67,14 +68,14 @@ def event_emax(t, x):
 event_emax.terminal = True
 
 def event_edt0(t, x):
-    return x[4]
+    return x[7]
 event_edt0.terminal = True
 
 
 class HeliSimulation(object):
     def __init__(self, theta1_0, theta2_0, theta3_0, timeStep):
         """Initializes the simulation"""
-        self.currentState = np.array([theta1_0, theta2_0, theta3_0, 0, 0, 0])
+        self.currentState = np.array([theta1_0, theta2_0, theta3_0, 0, 0, 0, 0, 0, 0])
         self.should_check_limits = True
         self.statLim = StateLimits()
         self.timeStep = timeStep
@@ -167,7 +168,7 @@ class HeliSimulation(object):
     #     self.solver.set_initial_value([p, e, lamb, dp, de, dlamb], self.solver.t)
     #     return
 
-    def rhs(self, t, x, V_s, V_d):
+    def rhs_old(self, t, x, V_s, V_d):
         """Right hand side of the differential equation being integrated"""
         p, e, lamb, dp, de, dlamb = x
 
@@ -189,6 +190,40 @@ class HeliSimulation(object):
 
         return np.array([dp, de, dlamb, ddp, dde, ddlamb])
 
+    def rhs(self, t, x, V_s, V_d):
+        """Right hand side of the differential equation being integrated"""
+        p, e, lamb, dp, de, dlamb, ddp_event, dde_event, ddlamb_event = x
+
+        # calculate dp, de and dlamb
+        if self.model_type == ModelType.EASY:
+            ddp = (L_1 / J_p) * V_d
+            dde = (L_2/J_e) * np.cos(e) + (L_3/J_e) * np.cos(p) * V_s
+            ddlamb = (L_4/J_l) * np.cos(e) * np.sin(p) * V_s
+
+        if self.model_type == ModelType.FRICTION:
+            ddp = (L_1 / J_p) * V_d - (mc.d_p / J_p) * dp
+            dde = (L_2/J_e) * np.cos(e) + (L_3/J_e) * np.cos(p) * V_s - (mc.d_e / J_e) * de
+            ddlamb = (L_4/J_l) * np.cos(e) * np.sin(p) * V_s - (mc.d_l / J_l) * dlamb
+
+        if self.model_type == ModelType.CENTRIPETAL:
+            ddp = (L_1 / J_p) * V_d - (mc.d_p / J_p) * dp + np.cos(p) * np.sin(p) * (de**2 - np.cos(e)**2 * dlamb**2)
+            dde = (L_2/J_e) * np.cos(e) + (L_3/J_e) * np.cos(p) * V_s - (mc.d_e / J_e) * de - np.cos(e) * np.sin(e) * dlamb**2
+            ddlamb = (L_4/J_l) * np.cos(e) * np.sin(p) * V_s - (mc.d_l / J_l) * dlamb
+
+        ddp_event = ddp
+        dde_event = dde
+        ddlamb_event = ddlamb_event
+
+        if self.statLim.p == LimitType.UPPER_LIMIT or self.statLim.p == LimitType.LOWER_LIMIT:
+            dp = 0
+            ddp = 0
+
+        if self.statLim.e == LimitType.UPPER_LIMIT or self.statLim.e == LimitType.LOWER_LIMIT:
+            de = 0
+            dde = 0
+
+        return np.array([dp, de, dlamb, ddp, dde, ddlamb, ddp_event, dde_event, ddlamb_event])
+
     def calcStep(self, V_f, V_b):
         """Returns the state of the system after the next time step
         V_f: voltage of the propeller right at back (of Fig.7) / first endeffector
@@ -207,25 +242,59 @@ class HeliSimulation(object):
         sol = scipy.integrate.solve_ivp(lambda t, x: self.rhs(t, x, V_s, V_d),
                                         (self.currentTime, self.currentTime+self.timeStep), self.currentState,
                                         method='RK45', t_eval=tt,  rtol=1e-6, atol=1e-9,
-                                        events=[event_pmin, event_pmax, event_pdt0,
-                                                event_emin, event_emax, event_edt0])
+                                        events=[event_pmin, event_pmax,
+                                                event_emin, event_emax, event_pdt0, event_edt0])
         #check if there was an event in order to limit the angle states
+        ##TODO: Simultion Time must always stop at +timeStep !
         if np.size(sol.t_events) != 0:
-            print(sol.t_events)
+            #1. Manage state machine
+            if np.size(sol.t_events[0]) != 0:
+                print("pmin")
+                self.statLim.p = LimitType.LOWER_LIMIT
+            if np.size(sol.t_events[1]) != 0:
+                print("pmax")
+                self.statLim.p = LimitType.UPPER_LIMIT
+            if np.size(sol.t_events[2]) != 0:
+                print("emin")
+                self.statLim.e = LimitType.LOWER_LIMIT
+            if np.size(sol.t_events[3]) != 0:
+                print("emax")
+                self.statLim.e = LimitType.UPPER_LIMIT
+            if np.size(sol.t_events[4]) != 0:
+                print("p  no limit")
+                self.statLim.p = LimitType.NO_LIMIT_REACHED
+            if np.size(sol.t_events[5]) != 0:
+                print("e  no limit")
+                self.statLim.e = LimitType.NO_LIMIT_REACHED
+
+            #2. Simulate until end of time interval
+            #Because of all events terminate the simulation it is clear that there only can be
+            #one event in the t_events array. Therefore we just have to look up this time value
+            self.currentTime =
+            sol2 = scipy.integrate.solve_ivp(lambda t, x: self.rhs(t, x, V_s, V_d),
+                                            (self.currentTime, self.currentTime + self.timeStep), self.currentState,
+                                            method='RK45', t_eval=tt, rtol=1e-6, atol=1e-9,
+                                            events=[event_pmin, event_pmax,
+                                                    event_emin, event_emax])
         self.currentState = sol.y[:, -1]
         self.currentTime = sol.t[-1]
         #end = time.time()
         #print(end - start)
-        return self.currentState
+        return self.currentState[0:6]
 
     def getCurrentState(self):
-        return self.currentState #yes, this is technically seen unnecessary, but accessing 'private' member objects from outside is rather ugly
+        return self.currentState[0:6]
 
     def getCurrentTime(self):
         return self.currentTime
 
     def setCurrentStateAndTime(self, state, time=0.0):
-        self.currentState = np.array(state)
+        state = np.array(state)
+        state = np.append(state, state[3:]) #convert outside state to inside state
+        #print("setCurrentStateandTime")
+        #print(state)
+        self.currentTime = time
+        self.currentState = state
 
     def setModelType(self, modelType):
         self.model_type = modelType
