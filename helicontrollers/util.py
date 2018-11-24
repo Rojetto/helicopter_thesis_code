16af3eed3e597bb74873bc3d79332e65c55a40e0
Robert Heedt <robert.heedt@gmail.com>
2018-11-24 15:16:41 +0100

    Add functions to compute feed forward controller outputs

--- helicontrollers/util.py
+++ helicontrollers/util.py
@@ -1,7 +1,16 @@
 from enum import Enum
 import ModelConstants as mc
-from numpy.ma import cos, sin
+from numpy.ma import cos, sin, arctan
 import numpy as np
+
+L1 = mc.l_p
+L2 = mc.g * (mc.l_c * mc.m_c - 2 * mc.l_h * mc.m_p)
+L3 = mc.l_h
+L4 = mc.l_h
+
+Jp = 2 * mc.m_p * mc.l_p ** 2
+Je = mc.m_c * mc.l_c ** 2 + 2 * mc.m_p * mc.l_h ** 2
+Jl = mc.m_c * mc.l_c ** 2 + 2 * mc.m_p * (mc.l_h ** 2 + mc.l_p ** 2)
 
 
 class ModelType(Enum):
@@ -36,20 +45,8 @@ def compute_linear_ss_and_op_output(model_type: ModelType, e_op):
     :param e_op: elevation angle (rad) of the desired operating point
     :return: A, B, Vf_op, Vb_op
     """
-    L1 = mc.l_p
-    L2 = mc.g * (mc.l_c * mc.m_c - 2 * mc.l_h * mc.m_p)
-    L3 = mc.l_h
-    L4 = mc.l_h
-
-    Jp = 2 * mc.m_p * mc.l_p ** 2
-    Je = mc.m_c * mc.l_c ** 2 + 2 * mc.m_p * mc.l_h ** 2
-    Jl = mc.m_c * mc.l_c ** 2 + 2 * mc.m_p * (mc.l_h ** 2 + mc.l_p ** 2)
-
-    Vs_op = - L2 / L3 * cos(e_op)
-    Vd_op = 0
-
-    Vf_op = (Vs_op + Vd_op) / 2
-    Vb_op = (Vs_op - Vd_op) / 2
+    Vf_op, Vb_op = compute_feed_forward_static([e_op, 0, 0, 0, 0], [0, 0, 0, 0, 0])
+    Vs_op = Vf_op + Vb_op
 
     if model_type == ModelType.EASY:
         A = np.array([[0, 0, 0, 1, 0, 0],
@@ -75,3 +72,60 @@ def compute_linear_ss_and_op_output(model_type: ModelType, e_op):
                   [0, 0]])
 
     return A, B, Vf_op, Vb_op
+
+
+def compute_feed_forward_static(e_and_derivatives, lambda_and_derivatives):
+    e = e_and_derivatives[0]
+    Vs = - L2 / L3 * cos(e)
+    Vd = 0
+
+    Vf = (Vs + Vd) / 2
+    Vb = (Vs - Vd) / 2
+
+    return Vf, Vb
+
+
+def compute_feed_forward_flatness(e_and_derivatives, lambda_and_derivatives):
+    e = e_and_derivatives[0]
+    de1 = e_and_derivatives[1]
+    de2 = e_and_derivatives[2]
+    de3 = e_and_derivatives[3]
+    de4 = e_and_derivatives[4]
+
+    l = lambda_and_derivatives[0]
+    dl1 = lambda_and_derivatives[1]
+    dl2 = lambda_and_derivatives[2]
+    dl3 = lambda_and_derivatives[3]
+    dl4 = lambda_and_derivatives[4]
+
+    b = L3 * Jl * dl2
+    c = L4 * cos(e)
+    d = Je * de2 - L2 * cos(e)
+    a = b * c / d
+
+    db1 = L3 * Jl * dl3
+    db2 = L3 * Jl * dl4
+    dc1 = - L4*sin(e)*de1
+    dc2 = - L4 * (cos(e) * de1**2 + sin(e) * de2)
+    dd1 = Je * de3 + L2 * sin(e) * de1
+    dd2 = Je * de4 + L2 * (cos(e) * de1**2 + sin(e) * de2)
+    f = db1 * c * d
+    g = dc1 * d + c * dd1
+    h = c * c * d * d
+    da1 = (f - b * g) / h
+
+    df1 = db2 * c * d + db1 * g
+    dg1 = dc2 * d + 2 * dc2 * d + c * dd2
+    dh1 = 2 * c * dc1 * d**2 + 2 * c**2 * d * dd1
+    da2 = ((df1 - (db1 * g + b * dg1)) * h - (f - b * g) * dh1) / h**2
+
+    p = arctan(a)
+    dp2 = (da2 * (1 + a ** 2) - 2 * a * da1 ** 2) / (1 + a**2)**2
+
+    Vs = Jl * dl2 / (L4 * cos(e) * sin(p))
+    Vd = Jp * dp2 / L1
+
+    Vf = (Vs + Vd) / 2
+    Vb = (Vs - Vd) / 2
+
+    return Vf, Vb
