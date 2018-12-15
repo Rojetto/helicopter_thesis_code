092a161f0949bcf687431a555fd05b602c573a04
cbilsing <clemensbil@web.de>
2018-12-15 16:08:46 +0100

    commit2

--- Observer.py
+++ Observer.py
@@ -4,8 +4,20 @@ from enum import Enum
 import numpy as np
 import abc # abstract base class
 import math
-import scipy as sp
-from scipy import special
+from ModelConstants import ModelType
+from helicontrollers.util import compute_linear_ss
+import scipy.linalg
+
+
+def discretize_linear_state_space(At, Bt, Ct, Dt, T):
+    """Calculates the discrete state space matrices Ak, Bk, Ck and Dk"""
+    n = At.shape[0] # it is assumed that At is quadratic
+    Ak = scipy.linalg.expm(At * T)
+    Bk = np.inv(At) * (Ak - np.eye(n)) * Bt
+    Ck = Ct
+    Dk = Dt
+    return Ak, Bk, Ck, Dk
+
 
 
 class Observer(object):
@@ -34,9 +46,23 @@ class LinearKalmanFilter(Observer):
         super().__init__(init_state)
         self.init_cov_matrix = init_cov_matrix
         self.operating_point = operating_point
+        self.Ak = np.zeros((6, 6))
+        self.Bk = np.zeros((6, 2))
+        self.Ck = np.zeros(6).reshape(1, 6)
+        self.Dk = np.zeros(2).reshape(1, 2)
+
+    def set_system_model_and_step_size(self, model_type: ModelType, stepSize):
+        """Linearizes and discretizes the current system model at the operating point
+        and saves it for calc_observation"""
+
+        At, Bt = compute_linear_ss(model_type, self.operating_point[1])
+        Ct = np.array([1, 1, 1, 0, 0, 0]).reshape((1, 6))
+        Dt = np.zeros(2).reshape(1, 2)
+
+        self.Ak, self.Bk, self.Ck, self.Dk = discretize_linear_state_space(At, Bt, Ct, Dt, stepSize)
+        return
 
     def calc_observation(self, t, x, u):
-        # print("Kalman Filter ist called!")
-
+        # print("Kalman Filter is called!")
         return np.zeros(6)
 
