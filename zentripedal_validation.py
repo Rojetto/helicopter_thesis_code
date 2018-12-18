ae2d479ab1ef09eaeeb5395c1af09800458e930e
Max Mustermann <m400275@mvrht.com>
2018-12-18 02:43:03 +0100

    validation of zentripedal forces with de = dp = 0 and ddl = 0

---
+++ zentripedal_validation.py
@@ -0,0 +1,76 @@
+import numpy as np
+import ModelConstants as mc
+from HeliSimulation import getInertia
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+
+def calculateInputs(e, p, rad=True):
+    """
+    calculates inputs Vs, Vd, dlambda for special case: ddlambda = 0, de = 0, dp = 0
+    """
+
+    if not rad:
+        e = e * np.pi / 180
+        p = p * np.pi / 180
+
+    L1 = mc.l_p
+    L2 = mc.g * (mc.l_c * mc.m_c - 2 * mc.l_h * mc.m_p)
+    L3 = mc.l_h
+    L4 = mc.l_h
+    J_p, J_e, J_l = getInertia([p, e, 0, 0, 0, 0, 0, 0], True)
+
+    P = - L3 * np.cos(p) * mc.d_l ** 2 / (J_e * L4 ** 2 * np.cos(e) ** 2 * np.sin(e) * np.sin(p))
+    Q = - L2 * mc.d_l ** 2 / (J_e * L4 ** 2 * np.cos(e) * np.sin(e) * np.sin(p))
+
+    z = P ** 2 / 4 - Q
+
+    if z < 0:
+        return [(np.NaN, np.NaN, np.NaN), (np.NaN, np.NaN, np.NaN)]
+
+    Vs1 = - P / 2 + np.sqrt(z)
+    Vs2 = - P / 2 - np.sqrt(z)
+
+    dl1 = Vs1 * L4 * np.cos(e) * np.sin(e) / mc.d_l
+    dl2 = Vs2 * L4 * np.cos(e) * np.sin(e) / mc.d_l
+
+    Vd1 = J_p * np.cos(p) * np.sin(p) * np.cos(e) * dl1 ** 2 / L1
+    Vd2 = J_p * np.cos(p) * np.sin(p) * np.cos(e) * dl2 ** 2 / L1
+
+    # return [(-0, -0, -0), (-0, -0, 0)]
+    return [(Vs1, dl1, Vd1), (Vs2, dl2, Vd2)]
+
+
+if __name__ == '__main__':
+    print(calculateInputs(60, 80, rad=False))
+
+    steps = 100
+    e = np.linspace(10, 80, steps)
+    e = np.linspace(100, 170, steps)
+    p = np.linspace(10, 80, steps)
+    em, pm = np.meshgrid(p, e)
+    Sol = np.empty((steps, steps, 2, 3))
+    for x in range(steps):
+        for y in range(steps):
+            Sol[x, y] = np.array([calculateInputs(e[x], p[y], rad=False)])
+
+    print(e.shape, p.shape, Sol.shape)
+
+    for solnr in [0, 1]:
+        fig1 = plt.figure()
+        ax = fig1.gca(projection='3d')
+        surf = ax.plot_surface(pm, em, Sol[:, :, solnr, 0])
+        ax.set(title='Vs', xlabel='e', ylabel='p', zlabel='Vs')
+
+        fig2 = plt.figure()
+        ax = fig2.gca(projection='3d')
+        surf = ax.plot_surface(pm, em, Sol[:, :, solnr, 2])
+        ax.set(title='Vd', xlabel='e', ylabel='p', zlabel='Vd')
+
+        fig3 = plt.figure()
+        ax = fig3.gca(projection='3d')
+        surf = ax.plot_surface(pm, em, Sol[:, :, solnr, 1])
+        ax.set(title='velocity of lambda', xlabel='e', ylabel='p', zlabel='dlambda')
+
+        plt.show()
