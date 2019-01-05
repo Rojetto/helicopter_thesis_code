6342ed907485330205362f5791f59a101ab8b5c7
Robert Heedt <robert.heedt@gmail.com>
2019-01-05 22:03:09 +0100

    Add lambda and epsilon interactive control mode

--- helicontrollers/InteractiveController.py
+++ helicontrollers/InteractiveController.py
@@ -9,7 +9,7 @@ import numpy as np
 class InteractiveMode(Enum):
     VD_VS = 0
     PHI_VS = 1
-    DLAMBDA_DE = 2
+    DLAMBDA_DEPSILON = 2
 
 
 class InteractiveController(AbstractController):
@@ -23,10 +23,14 @@ class InteractiveController(AbstractController):
         self.mode = InteractiveMode.VD_VS
         self.rotor_speed_pd = [5, 0]
         self.phi_pd_gains = [20, 2]
+        self.dlambda_pd_gains = [0, 0.5]
+        self.depsilon_pd_gains = [15, 5]
 
         self.front_rotor_pd = None
         self.back_rotor_pd = None
         self.phi_pd = None
+        self.dlambda_pd = None
+        self.depsilon_pd = None
 
         self.joystick_widget = joystick_widget
         self.joystick_widget.pos_changed.connect(self.on_pos_changed)
@@ -34,12 +38,17 @@ class InteractiveController(AbstractController):
         self.x = 0
         self.y = 0
 
+        self.target_lambda = 0
+        self.target_epsilon = 0
+
         super().__init__("Interactive control", {
             "Mode": ParamEnum(["Vd, Vs", "phi, Vs", "dlambda, depsilon"],
-                              [InteractiveMode.VD_VS, InteractiveMode.PHI_VS, InteractiveMode.DLAMBDA_DE],
+                              [InteractiveMode.VD_VS, InteractiveMode.PHI_VS, InteractiveMode.DLAMBDA_DEPSILON],
                               self.mode),
             "Rotor speed PD": ParamFloatArray([0, 0], [100, 100], self.rotor_speed_pd),
-            "Phi PD": ParamFloatArray([0, 0], [100, 100], self.phi_pd_gains)
+            "Phi PD": ParamFloatArray([0, 0], [100, 100], self.phi_pd_gains),
+            "dlambda PD": ParamFloatArray([0, 0], [100, 100], self.dlambda_pd_gains),
+            "depsilon PD": ParamFloatArray([0, 0], [100, 100], self.depsilon_pd_gains)
         })
 
     def on_pos_changed(self, x, y):
@@ -62,8 +71,22 @@ class InteractiveController(AbstractController):
             Vs = self.y
             phi_d = self.x
 
-            p_error = x[0] - phi_d
+            p_error = p - phi_d
             Vd = - self.phi_pd.compute(t, p_error, dp)
+
+            Vf = (Vs + Vd) / 2
+            Vb = (Vs - Vd) / 2
+        elif self.mode == InteractiveMode.DLAMBDA_DEPSILON:
+            dlambda_d = self.x
+            depsilon_d = self.y
+
+            self.target_epsilon += 1 / 60.0 * depsilon_d
+            self.target_lambda += 1 / 60.0 * dlambda_d
+
+            phi_d = self.dlambda_pd.compute(t, self.target_lambda - l, dlambda_d - dl)
+            Vd = - self.phi_pd.compute(t, p - phi_d, dp)
+
+            Vs = self.depsilon_pd.compute(t, self.target_epsilon - e, depsilon_d - de)
 
             Vf = (Vs + Vd) / 2
             Vb = (Vs - Vd) / 2
@@ -77,10 +100,14 @@ class InteractiveController(AbstractController):
         self.mode = param_value_dict["Mode"]
         self.rotor_speed_pd = param_value_dict["Rotor speed PD"]
         self.phi_pd_gains = param_value_dict["Phi PD"]
+        self.dlambda_pd_gains = param_value_dict["dlambda PD"]
+        self.depsilon_pd_gains = param_value_dict["depsilon PD"]
 
         self.front_rotor_pd = PidAlgorithm([self.rotor_speed_pd[0], 0, self.rotor_speed_pd[1]])
         self.back_rotor_pd = PidAlgorithm([self.rotor_speed_pd[0], 0, self.rotor_speed_pd[1]])
         self.phi_pd = PidAlgorithm([self.phi_pd_gains[0], 0, self.phi_pd_gains[1]])
+        self.dlambda_pd = PidAlgorithm([self.dlambda_pd_gains[0], 0, self.dlambda_pd_gains[1]])
+        self.depsilon_pd = PidAlgorithm([self.depsilon_pd_gains[0], 0, self.depsilon_pd_gains[1]])
 
         if self.mode == InteractiveMode.VD_VS:
             self.joystick_widget.real_x_min = -self.VD_MAX
@@ -92,10 +119,12 @@ class InteractiveController(AbstractController):
             self.joystick_widget.real_x_max = self.PHI_MAX
             self.joystick_widget.real_y_min = 0
             self.joystick_widget.real_y_max = self.VS_MAX
-        elif self.mode == InteractiveMode.DLAMBDA_DE:
+        elif self.mode == InteractiveMode.DLAMBDA_DEPSILON:
             self.joystick_widget.real_x_min = -self.DLAMBDA_MAX
             self.joystick_widget.real_x_max = self.DLAMBDA_MAX
             self.joystick_widget.real_y_min = -self.DEPSILON_MAX
             self.joystick_widget.real_y_max = self.DEPSILON_MAX
 
+        self.target_lambda = 0
+        self.target_epsilon = 0
         self.joystick_widget.reset_pos()
