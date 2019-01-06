8b55251e66a1b37a164d99fd33cf13eacfdddb55
Robert Heedt <robert.heedt@gmail.com>
2019-01-06 14:04:24 +0100

    Add direct Vf, Vb control Show angles and angular velocities in joystick widget in degrees

--- helicontrollers/InteractiveController.py
+++ helicontrollers/InteractiveController.py
@@ -10,17 +10,19 @@ class InteractiveMode(Enum):
     VD_VS = 0
     PHI_VS = 1
     DLAMBDA_DEPSILON = 2
+    VF_VB = 3
 
 
 class InteractiveController(AbstractController):
     VD_MAX = 0.5
-    VS_MAX = 1
-    PHI_MAX = 80 / 180 * np.pi
-    DLAMBDA_MAX = 90 / 180 * np.pi
-    DEPSILON_MAX = 90 / 180 * np.pi
+    VS_MAX = 1.0
+    VFB_MAX = 0.5
+    PHI_MAX = 80
+    DLAMBDA_MAX = 90
+    DEPSILON_MAX = 90
 
     def __init__(self, joystick_widget: JoystickWidget):
-        self.mode = InteractiveMode.VD_VS
+        self.mode = InteractiveMode.VF_VB
         self.rotor_speed_pd = [5, 0]
         self.phi_pd_gains = [20, 2]
         self.dlambda_pd_gains = [0, 0.5]
@@ -42,8 +44,8 @@ class InteractiveController(AbstractController):
         self.target_epsilon = 0
 
         super().__init__("Interactive control", {
-            "Mode": ParamEnum(["Vd, Vs", "phi, Vs", "dlambda, depsilon"],
-                              [InteractiveMode.VD_VS, InteractiveMode.PHI_VS, InteractiveMode.DLAMBDA_DEPSILON],
+            "Mode": ParamEnum(["Vf, Vb", "Vd, Vs", "phi, Vs", "dlambda, depsilon"],
+                              [InteractiveMode.VF_VB, InteractiveMode.VD_VS, InteractiveMode.PHI_VS, InteractiveMode.DLAMBDA_DEPSILON],
                               self.mode),
             "Rotor speed PD": ParamFloatArray([0, 0], [100, 100], self.rotor_speed_pd),
             "Phi PD": ParamFloatArray([0, 0], [100, 100], self.phi_pd_gains),
@@ -61,7 +63,10 @@ class InteractiveController(AbstractController):
         Vf = 0
         Vb = 0
 
-        if self.mode == InteractiveMode.VD_VS:
+        if self.mode == InteractiveMode.VF_VB:
+            Vf = self.x
+            Vb = self.y
+        elif self.mode == InteractiveMode.VD_VS:
             Vd = self.x
             Vs = self.y
 
@@ -69,7 +74,7 @@ class InteractiveController(AbstractController):
             Vb = (Vs - Vd) / 2
         elif self.mode == InteractiveMode.PHI_VS:
             Vs = self.y
-            phi_d = self.x
+            phi_d = self.x / 180 * np.pi
 
             p_error = p - phi_d
             Vd = - self.phi_pd.compute(t, p_error, dp)
@@ -77,8 +82,8 @@ class InteractiveController(AbstractController):
             Vf = (Vs + Vd) / 2
             Vb = (Vs - Vd) / 2
         elif self.mode == InteractiveMode.DLAMBDA_DEPSILON:
-            dlambda_d = self.x
-            depsilon_d = self.y
+            dlambda_d = self.x / 180 * np.pi
+            depsilon_d = self.y / 180 * np.pi
 
             self.target_epsilon += 1 / 60.0 * depsilon_d
             self.target_lambda += 1 / 60.0 * dlambda_d
@@ -109,7 +114,12 @@ class InteractiveController(AbstractController):
         self.dlambda_pd = PidAlgorithm([self.dlambda_pd_gains[0], 0, self.dlambda_pd_gains[1]])
         self.depsilon_pd = PidAlgorithm([self.depsilon_pd_gains[0], 0, self.depsilon_pd_gains[1]])
 
-        if self.mode == InteractiveMode.VD_VS:
+        if self.mode == InteractiveMode.VF_VB:
+            self.joystick_widget.real_x_min = 0
+            self.joystick_widget.real_x_max = self.VFB_MAX
+            self.joystick_widget.real_y_min = 0
+            self.joystick_widget.real_y_max = self.VFB_MAX
+        elif self.mode == InteractiveMode.VD_VS:
             self.joystick_widget.real_x_min = -self.VD_MAX
             self.joystick_widget.real_x_max = self.VD_MAX
             self.joystick_widget.real_y_min = 0
