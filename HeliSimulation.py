added:      * angle limits for 5-output-gyromoment filter      * covariance matrix is now available in the logger file fixed:      * small bug with the visualization of the estimated state

HeliSimulation.py
@@ -33,9 +33,10 @@ class StateLimits(object):
     e_min = -e_max
     lamb_max = 120 / 180.0 * np.pi
     lamb_min = -lamb_max
-    eps_p = 0
-    eps_e = 0
-    eps_lamb = 0
+    # these epsillon values are necessary for set_current_state_and_time_and_check_limits
+    eps_p = 1 / 180.0 * np.pi
+    eps_e = 1 / 180.0 * np.pi
+    eps_lamb = 1 / 180.0 * np.pi
 
     def __init__(self):
         self.p = LimitType.NO_LIMIT_REACHED
@@ -443,6 +444,115 @@ class HeliSimulation(object):
         self.statLim.p = LimitType.NO_LIMIT_REACHED
         self.statLim.e = LimitType.NO_LIMIT_REACHED
         self.statLim.lamb = LimitType.NO_LIMIT_REACHED
+
+    # def set_current_state_and_time_and_check_limits(self, state, sim_time=0.0):
+    #     """state = p, e, lamb, dp, de, dlamb, f_speed, b_speed
+    #     This function takes also care of not letting the values be beyond the angle limits
+    #     :return corrected_state: this function returns the new state which is definitely inside of the allowed
+    #                              value range"""
+    #     p, e, lamb, dp, de, dlamb, f_speed, b_speed = state
+    #
+    #     # check if the new state values exceed the value range
+    #     # if this is the case then don't set the new state variables exactly on the limits
+    #     # because then the event system wouldn't work anymore as expected
+    #     if self.should_check_limits:
+    #         # pitch angle
+    #         if p > self.statLim.p_max:
+    #             p = self.statLim.p_max - self.statLim.eps_p
+    #         elif p < self.statLim.p_min:
+    #             p = self.statLim.p_min + self.statLim.eps_p
+    #         # elevation angle
+    #         if e > self.statLim.e_max:
+    #             e = self.statLim.e_max - self.statLim.eps_e
+    #         elif e < self.statLim.e_min:
+    #             e = self.statLim.e_min + self.statLim.eps_e
+    #         # travel angle
+    #         if lamb > self.statLim.lamb_max:
+    #             lamb = self.statLim.lamb_max - self.statLim.eps_lamb
+    #         elif lamb < self.statLim.lamb_min:
+    #             lamb = self.statLim.lamb_min + self.statLim.eps_lamb
+    #
+    #     corrected_state = np.array([p, e, lamb, dp, de, dlamb, f_speed, b_speed])
+    #     self.currentTime = sim_time
+    #     self.currentState = corrected_state
+    #     # reset state machine
+    #     self.statLim.p = LimitType.NO_LIMIT_REACHED
+    #     self.statLim.e = LimitType.NO_LIMIT_REACHED
+    #     self.statLim.lamb = LimitType.NO_LIMIT_REACHED
+    #     return corrected_state
+
+
+    def get_limited_state_and_change_state(self, state):
+        '''This method checks if the given state is out the allowed value range.
+        If this is the case, then the discrete state of the simulation is set accordingly. Furthermore
+        this method then returns the corrected state vector so that the kalman filter and the
+        simulation keep being synchronized. Also, this function alters the continuous state vector'''
+        p, e, lamb, dp, de, dlamb, f_speed, b_speed = state
+
+        # check if the new state values exceed the value range
+        # if this is the case then don't set the new state variables exactly on the limits
+        # because then the event system wouldn't work anymore as expected
+        if self.should_check_limits:
+            #   Check for angle being reached
+            # pitch angle
+            if p > self.statLim.p_max and self.statLim.p != LimitType.UPPER_LIMIT:
+                # equivalent to event_pmax
+                p = self.statLim.p_max
+                dp = 0
+                self.statLim.p = LimitType.UPPER_LIMIT
+            elif p < self.statLim.p_min and self.statLim.p != LimitType.LOWER_LIMIT:
+                # equivalent to event_pmin
+                p = self.statLim.p_min
+                dp = 0
+                self.statLim.p = LimitType.LOWER_LIMIT
+            # elevation angle
+            if e > self.statLim.e_max and self.statLim.e != LimitType.UPPER_LIMIT:
+                # equivalent to event_emax
+                e = self.statLim.e_max
+                de = 0
+                self.statLim.e = LimitType.UPPER_LIMIT
+            elif e < self.statLim.e_min and self.statLim.e != LimitType.LOWER_LIMIT:
+                # equivalent to event_emin
+                e = self.statLim.e_min
+                de = 0
+                self.statLim.e = LimitType.LOWER_LIMIT
+            # travel angle
+            if lamb > self.statLim.lamb_max and self.statLim.lamb != LimitType.UPPER_LIMIT:
+                # equivalent to event_lambmax
+                lamb = self.statLim.lamb_max
+                dlamb = 0
+                self.statLim.lamb = LimitType.UPPER_LIMIT
+            elif lamb < self.statLim.lamb_min and self.statLim.lamb != LimitType.LOWER_LIMIT:
+                # equivalent to event_lambmin
+                lamb = self.statLim.lamb_min
+                dlamb = 0
+                self.statLim.lamb = LimitType.LOWER_LIMIT
+
+            # check for leaving limit range
+            # pitch angle
+            if self.statLim.p == LimitType.UPPER_LIMIT and p < self.statLim.p_max:
+                self.statLim.p = LimitType.NO_LIMIT_REACHED
+            elif self.statLim.p == LimitType.LOWER_LIMIT and p > self.statLim.p_min:
+                self.statLim.p = LimitType.NO_LIMIT_REACHED
+            # elevation angle
+            if self.statLim.e == LimitType.UPPER_LIMIT and e < self.statLim.e_max:
+                self.statLim.e = LimitType.NO_LIMIT_REACHED
+            elif self.statLim.e == LimitType.LOWER_LIMIT and e > self.statLim.e_min:
+                self.statLim.e = LimitType.NO_LIMIT_REACHED
+            # travel angle
+            if self.statLim.lamb == LimitType.UPPER_LIMIT and lamb < self.statLim.lamb_max:
+                self.statLim.lamb = LimitType.NO_LIMIT_REACHED
+            elif self.statLim.lamb == LimitType.LOWER_LIMIT and lamb > self.statLim.lamb_min:
+                self.statLim.lamb = LimitType.NO_LIMIT_REACHED
+
+        corrected_state = np.array([p, e, lamb, dp, de, dlamb, f_speed, b_speed])
+        self.currentState = corrected_state
+        # reset state machine
+        self.statLim.p = LimitType.NO_LIMIT_REACHED
+        self.statLim.e = LimitType.NO_LIMIT_REACHED
+        self.statLim.lamb = LimitType.NO_LIMIT_REACHED
+        return corrected_state
+
 
     def set_model_type(self, modelType):
         self.model_type = modelType

Observer.py
@@ -158,7 +158,8 @@ class Observer(object):
                     Returns:
                         x_hat: estimated state of system
                         noisy_input: input signal with noise
-                        noisy_output; output signal with noise"""
+                        noisy_output; output signal with noise
+                        cov_matrix: current covariance matrix (8x8)"""
         return
 
     def set_estimated_state(self, x_estimated_state):
@@ -187,7 +188,7 @@ class KalmanFilterBase(Observer):
         self.model_type = model_type
 
         self.bInputNoise = True
-        self.bOutputNoise = False
+        self.bOutputNoise = True
 
         # set system noise parameters
         if self.bInputNoise:
@@ -284,7 +285,7 @@ class NoKalmanFilter(KalmanFilterBase):
         u_with_noise = self.get_noisy_input_of_system(u)
         y_with_noise = self.get_noisy_output_of_system(x[0:3])
         y_with_noise = np.pad(y_with_noise, (0, 2), "constant")
-        return np.zeros(8), u_with_noise, y_with_noise
+        return x, u_with_noise, y_with_noise, np.zeros((8, 8))
 
     def set_system_model_and_step_size(self, model_type: ModelType, stepSize):
         """nothing here"""
@@ -298,7 +299,7 @@ class TestKalmanFilter(KalmanFilterBase):
         x_estimated = x
         u_with_noise = self.get_noisy_input_of_system(u)
         y_with_noise = self.get_noisy_output_of_system(x[0:3])
-        return x_estimated, u_with_noise, y_with_noise
+        return x_estimated, u_with_noise, y_with_noise, np.zeros((8, 8))
 
     def set_system_model_and_step_size(self, model_type: ModelType, stepSize):
         return
@@ -307,8 +308,6 @@ class TestKalmanFilter(KalmanFilterBase):
 class LinearKalmanFilter(KalmanFilterBase):
     """This kalman filter linearizes the model in a fixed operating point which is defined at initialisation."""
 
-    # ToDo: perhaps split up this class into two (LinearKalmanFilterSimple and LinearKalmanFilterGyro)
-    # ToDo: because of all the if statements it has already become a bit difficult to read
     def __init__(self, init_state, init_cov_matrix, model_type : ModelType, e_op, lamb_op, nOutputs, stepSize):
         ''':arg operating_point: 8-element-vector'''
         super().__init__(init_state, init_cov_matrix, model_type, nOutputs)
@@ -451,8 +450,12 @@ class LinearKalmanFilter(KalmanFilterBase):
             x_estimated_state = np.resize(x_estimated_state, (1, 8))[0]
             if self.nOutputs == 3:
                 y_with_noise = np.pad(y_with_noise, (0, 2), "constant")
-        # add operating point for real state
-        return x_estimated_state, u_with_noise, y_with_noise
+        # 4. pad covariance matrix with zeros so that it has the shape 8x8
+        if self.nStateVars == 6:
+            cov_matrix_padded = np.pad(self.cov_matrix, ((0, 2), (0, 2)), "constant")
+        elif self.nStateVars == 8:
+            cov_matrix_padded = self.cov_matrix
+        return x_estimated_state, u_with_noise, y_with_noise, cov_matrix_padded
 
 
 
@@ -536,7 +539,9 @@ class ExtKalmanFilterEasyModel(KalmanFilterBase):
         x_estimated_state = np.resize(x_estimated_state, (1, 6))[0]
         x_estimated_state = np.pad(x_estimated_state, (0, 2), "constant")
         y_with_noise = np.pad(y_with_noise, (0, 2), "constant")
-        return x_estimated_state, u_with_noise, y_with_noise
+        # 4. pad covariance matrix with zeros so that it has the shape 8x8
+        cov_matrix_padded = np.pad(self.cov_matrix, ((0, 2), (0, 2)), "constant")
+        return x_estimated_state, u_with_noise, y_with_noise, cov_matrix_padded
 
 
 class ExtKalmanFilterGyroModel(KalmanFilterBase):
@@ -623,7 +628,7 @@ class ExtKalmanFilterGyroModel(KalmanFilterBase):
         x_estimated_state = self.ekf_algorithm(np.resize(u_with_noise, (2, 1)), np.resize(y_with_noise, (5, 1)))
         # 3. add two zero elements at the end of the state vector and at the end of the y_with_noise_vector
         x_estimated_state = np.resize(x_estimated_state, (1, 8))[0]
-        return x_estimated_state, u_with_noise, y_with_noise
+        return x_estimated_state, u_with_noise, y_with_noise, self.cov_matrix
 
 
 class ExtKalmanFilterGyroModelLimits(KalmanFilterBase):
@@ -695,15 +700,11 @@ class ExtKalmanFilterGyroModelLimits(KalmanFilterBase):
         cov_matrix_before = self.cov_matrix
         #     1. Prediction
         # predict the state by integrate the time continuous system numerically
-        # ToDo: check for limits when setting the new state
-        self.heliSim.set_current_state_and_time(self.x_estimated_state.reshape((1, 8))[0],
-                                                self.heliSim.get_current_time())
         sim_state_predict = self.heliSim.calc_step(u[0][0], u[1][0], self.no_disturbance_eval)
         x_est_predict = np.resize(sim_state_predict, (8, 1))
         # predict the new covariance by linearizing and discretizing the model
         Ak, Bk, Ck, Dk = self.get_linear_discrete_matrices(x_est_before, u[0][0], u[1][0])
         cov_matrix_predict = Ak @ cov_matrix_before @ np.transpose(Ak) + Bk @ self.N @ np.transpose(Bk)
-
         #     2. Update
         # compute kalman gain
         Kl = cov_matrix_predict @ np.transpose(Ck) @ np.linalg.inv(Ck @ cov_matrix_predict @ np.transpose(Ck) + self.W)
@@ -712,6 +713,12 @@ class ExtKalmanFilterGyroModelLimits(KalmanFilterBase):
         x_est_update = x_est_predict + Kl @ (y - y_est)
         # update covariance matrix (identity matrix must have as many lines as the Kalman gain
         cov_matrix_update = (np.eye(np.size(Kl, 0)) - Kl @ Ck) @ cov_matrix_predict
+
+        # check if the update step state needs to be changed because of limit crossing
+        # if that is the case, correct the state and set the state of the simulation accordingly
+        corrected_state = self.heliSim.get_limited_state_and_change_state(x_est_update.reshape((1, 8))[0])
+        x_est_update = np.resize(corrected_state, (8, 1))
+
         self.x_estimated_state = x_est_update
         self.cov_matrix = cov_matrix_update
         # print("------")
@@ -726,7 +733,7 @@ class ExtKalmanFilterGyroModelLimits(KalmanFilterBase):
         x_estimated_state = self.ekf_algorithm(np.resize(u_with_noise, (2, 1)), np.resize(y_with_noise, (5, 1)))
         # 3. add two zero elements at the end of the state vector and at the end of the y_with_noise_vector
         x_estimated_state = np.resize(x_estimated_state, (1, 8))[0]
-        return x_estimated_state, u_with_noise, y_with_noise
+        return x_estimated_state, u_with_noise, y_with_noise, self.cov_matrix
 
 
 class ExtKalmanFilterGyroModelOnly3(KalmanFilterBase):
@@ -810,7 +817,7 @@ class ExtKalmanFilterGyroModelOnly3(KalmanFilterBase):
         # 3. add two zero elements at the end of the state vector and at the end of the y_with_noise_vector
         x_estimated_state = np.resize(x_estimated_state, (1, 8))[0]
         y_with_noise = np.pad(y_with_noise, (0, 2), "constant")
-        return x_estimated_state, u_with_noise, y_with_noise
+        return x_estimated_state, u_with_noise, y_with_noise, self.cov_matrix
 
 
 # class LinearKalmanFilter(Observer):
@@ -854,7 +861,6 @@ class ExtKalmanFilterGyroModelOnly3(KalmanFilterBase):
 #     def get_output_of_system(self, x):
 #         """Calculates the output from the current state and the Ck-Matrix + adds gaussian noise"""
 #         y = self.Ck @ x
-#         # ToDo: Werte anzeigen
 #         y[0] += np.random.normal(0, np.sqrt(self.p_noise), 1)[0]
 #         y[1] += np.random.normal(0, np.sqrt(self.e_noise), 1)[0]
 #         y[2] += np.random.normal(0, np.sqrt(self.lamb_noise), 1)[0]

logger.py
@@ -18,6 +18,7 @@ xs_store = np.empty((current_size, 8))
 xs_estimated_store = np.empty((current_size, 8))
 ys_noisy_output_store = np.empty((current_size, 5))
 us_noisy_input_store = np.empty((current_size, 2))
+cov_matrix_store = np.empty((current_size, 8, 8))
 us_ff_store = np.empty((current_size, 2))
 us_controller_store = np.empty((current_size, 2))
 e_traj_store = np.empty((current_size, 5))
@@ -28,7 +29,7 @@ planner_elevation_g = 0
 
 class LoggingDataV2:
     def __init__(self, ts, xs, us_ff, us_controller, e_traj_and_derivatives, lambda_traj_and_derivatives,
-                 xs_estimated_state, us_noisy_input, ys_noisy_output):
+                 xs_estimated_state, us_noisy_input, ys_noisy_output, cov_matrix):
         self.ts = ts
         self.xs = xs
         self.us_ff = us_ff
@@ -38,6 +39,7 @@ class LoggingDataV2:
         self.xs_estimated_state = xs_estimated_state
         self.ys_noisy_output = ys_noisy_output
         self.us_noisy_input = us_noisy_input
+        self.cov_matrix = cov_matrix
 
 
 class LoggingDataV1:
@@ -55,7 +57,7 @@ def bundle_data():
 
     bundle = LoggingDataV2(ts_store[:index], xs_store[:index], us_ff_store[:index], us_controller_store[:index],
                            e_traj_store[:index], lambda_traj_store[:index], xs_estimated_store[:index],
-                           us_noisy_input_store[:index], ys_noisy_output_store[:index])
+                           us_noisy_input_store[:index], ys_noisy_output_store[:index], cov_matrix_store[:index])
 
     return bundle
 
@@ -84,7 +86,7 @@ def add_planner(planner_travel, planner_elevation):
 
 
 def add_frame(t, x, u_ff, u_controller, e_traj_and_derivatives, lambda_traj_and_derivatives, x_estimated_state,
-              us_noisy_input, ys_noisy_output):
+              us_noisy_input, ys_noisy_output, cov_matrix):
     global current_size, index
 
     if index == current_size:
@@ -98,6 +100,7 @@ def add_frame(t, x, u_ff, u_controller, e_traj_and_derivatives, lambda_traj_and_
         lambda_traj_store.resize((current_size, 5), refcheck=False)
         ys_noisy_output_store.resize((current_size, 5), refcheck=False)
         us_noisy_input_store.resize((current_size, 2), refcheck=False)
+        cov_matrix_store.resize((current_size, 8, 8), refcheck=False)
 
     ts_store[index] = t
     xs_store[index] = x
@@ -108,6 +111,7 @@ def add_frame(t, x, u_ff, u_controller, e_traj_and_derivatives, lambda_traj_and_
     us_noisy_input_store[index] = us_noisy_input
     e_traj_store[index] = e_traj_and_derivatives
     lambda_traj_store[index] = lambda_traj_and_derivatives
+    cov_matrix_store[index] = cov_matrix
 
     index += 1
 
@@ -134,6 +138,7 @@ def process(bundle: LoggingDataV2):
     xs_estimated_state = bundle.xs_estimated_state
     us_noisy_input = bundle.us_noisy_input
     ys_noisy_output = bundle.ys_noisy_output
+    cov_matrix = bundle.cov_matrix
 
     # Your data processing code goes here
 
@@ -147,13 +152,13 @@ def process(bundle: LoggingDataV2):
     # ax1.grid()
     # plt.show()
 
-    # plotMoments(bundle)
+    plotMoments(bundle)
 
     plotBasics(bundle)
 
-    # plotValidation(bundle)
+    plotValidation(bundle)
 
-    # plotInputs(bundle)
+    plotInputs(bundle)
 
     plotObserver(bundle)
 
@@ -350,6 +355,24 @@ def plotObserver(bundle):
     xs_estimated_state = bundle.xs_estimated_state
     us_noisy_input = bundle.us_noisy_input
     ys_noisy_output = bundle.ys_noisy_output
+    cov_matrix = bundle.cov_matrix
+
+    fig = custom_figure("Covariance Matrix (var(p), var(e), var(lambda))")
+    plt.plot(ts, cov_matrix[:, 0, 0], label="var(p)")
+    plt.plot(ts, cov_matrix[:, 1, 1], label="var(e)")
+    plt.plot(ts, cov_matrix[:, 2, 2], label="var(lambda)")
+    plt.legend()
+
+    fig = custom_figure("Covariance Matrix (var(dp), var(de), var(dlambda))")
+    plt.plot(ts, cov_matrix[:, 3, 3], label="var(dp)")
+    plt.plot(ts, cov_matrix[:, 4, 4], label="var(de)")
+    plt.plot(ts, cov_matrix[:, 5, 5], label="var(dlambda)")
+    plt.legend()
+
+    fig = custom_figure("Covariance Matrix (var(f), var(b))")
+    plt.plot(ts, cov_matrix[:, 6, 6], label="var(f)")
+    plt.plot(ts, cov_matrix[:, 7, 7], label="var(b)")
+    plt.legend()
 
     fig = custom_figure("Estimated state of observer (p, e, lambda)")
     plt.plot(ts, xs_estimated_state[:, 0], label="p observed")