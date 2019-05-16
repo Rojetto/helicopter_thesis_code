2edf1232189d04e27e1439441a54f621aca53714
Robert Heedt <robert.heedt@gmail.com>
2019-05-16 17:28:19 +0200

    Add PID mode that calculates derivative via finite differences

--- matlab/pidAlgorithm.m
+++ matlab/pidAlgorithm.m
@@ -18,18 +18,22 @@ classdef pidAlgorithm < handle
             dt = t - obj.last_t;
             obj.ix = obj.ix + dt * x;
             
-%             if ~exist('dx', 'var')
-%                 if isempty(obj.last_x)
-%                     dx = 0;
-%                 else
-%                     dx = (x - obj.last_x) / dt;
-%                 end
-%             end
-            
             out = double(obj.gains(1)*x + obj.gains(2)*obj.ix + obj.gains(3)*dx);
             
             obj.last_t = t;
             obj.last_x = x;
+        end
+        
+        function out = compute_fd(obj, t, x)
+            dt = t - obj.last_t;
+            
+            if isempty(obj.last_x)
+                dx = 0;
+            else
+                dx = (x - obj.last_x) / dt;
+            end
+            
+            out = obj.compute(t, x, dx);
         end
     end
 end
