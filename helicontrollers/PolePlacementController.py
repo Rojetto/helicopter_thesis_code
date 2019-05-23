a1a225edc815ce38c366e7b96b9e2114fd60940e
Robert Heedt <robert.heedt@gmail.com>
2019-05-23 13:07:59 +0200

    Fix Python PolePlacementController

--- helicontrollers/PolePlacementController.py
+++ helicontrollers/PolePlacementController.py
@@ -13,6 +13,8 @@ class PolePlacementController(AbstractController):
         self.feedback_computed = False
         self.time_variant_feedback = False
 
+        self.trajectory = None
+
         super().__init__("Pole placement", {
             "Model type": ParamEnum(["Simple", "Friction", "Centripetal"],
                                     [ModelType.EASY, ModelType.FRICTION, ModelType.CENTRIPETAL],
@@ -21,24 +23,30 @@ class PolePlacementController(AbstractController):
             "Poles": ParamFloatArray(self.poles)
         })
 
-    def control(self, t, x, e_traj, lambda_traj):
+    def control(self, t, x):
         # delete front and back speed because we dont need it here
         x = x[:6]
+
+        traj_eval = self.trajectory.eval(t)
         if self.time_variant_feedback or not self.feedback_computed:
-            self.K = self.compute_feedback_matrix(e_traj[0])
+            self.K = self.compute_feedback_matrix(traj_eval.eps[0])
             self.feedback_computed = True
 
         # TODO: We could potentially also compute the trajectory for the pitch, should we do that? This would make the
         #       linearization a whole lot more complex though. Same for the derivatives of all states.
-        x_op = np.array([0, e_traj[0], lambda_traj[0], 0, 0, 0])
+        x_op = np.array([0, traj_eval.eps[0], traj_eval.lamb[0], 0, 0, 0])
+        u_op = np.array([traj_eval.vf[0], traj_eval.vb[0]])
 
-        u = - self.K @ (x - x_op)
+        u = u_op - self.K @ (x - x_op)
         return u
 
-    def initialize(self, param_value_dict):
+    def set_params(self, param_value_dict):
         self.model_type = param_value_dict["Model type"]
         self.poles = param_value_dict["Poles"]
         self.time_variant_feedback = param_value_dict["Time variant feedback"]
+
+    def initialize(self, trajectory):
+        self.trajectory = trajectory
         self.feedback_computed = False
 
     def compute_feedback_matrix(self, e_op):
