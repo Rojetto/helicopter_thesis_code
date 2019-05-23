5186d65565b1fe13e25328e94f4d5b38d2943c6f
Robert Heedt <robert.heedt@gmail.com>
2019-05-23 10:59:24 +0200

    Make TimeVariantLQR work

--- matlab/TimeVariantLQR.m
+++ matlab/TimeVariantLQR.m
@@ -4,11 +4,10 @@ classdef TimeVariantLQR < HeliController
         R
         R_inv
         S
-        ref_t
-        ref_u
-        ref_x
         riccati_tau
         riccati_P_triu_tau
+        
+        trajectory
     end
     
     properties (Nontunable)
@@ -20,30 +19,44 @@ classdef TimeVariantLQR < HeliController
     
     methods
         function obj = TimeVariantLQR()
+            obj.trajectory = Trajectory([], [], [], [], [], []);
+            
+            obj.Q = zeros(6);
+            obj.R = zeros(2);
+            obj.R_inv = zeros(2);
+            obj.S = zeros(6);
+            
+            obj.riccati_tau = [];
+            obj.riccati_P_triu_tau = [];
         end
         
-        function initialize(obj)
+        function initialize(obj, trajectory)
+            obj.trajectory = trajectory;
+            
             obj.Q = diag(obj.Q_diag);
             obj.R = diag(obj.R_diag);
             obj.R_inv = inv(obj.R);
             obj.S = diag(obj.S_diag);
             
-            mat_content = coder.load('move1.mat');
-            obj.ref_t = mat_content.t;
-            obj.ref_u = mat_content.u;
-            obj.ref_x = mat_content.x;
-            
             obj.solve_riccati()
         end
         
-        function out = control(obj, t, x, elevation_traj, travel_traj)
+        function out = control(obj, t, x)
             x = reshape(x(1:6), 6, 1);
-            te = obj.ref_t(end);
+            te = obj.trajectory.t(end);
             if t > te
                 t = te;
             end
-            u_d = interp1(obj.ref_t, obj.ref_u, t)';
-            x_d = interp1(obj.ref_t, obj.ref_x, t)';
+            traj_eval = obj.trajectory.eval(t);
+            u_d = [traj_eval.vf(1); traj_eval.vb(1)];
+            x_d = [
+                traj_eval.phi(1); 
+                traj_eval.eps(1); 
+                traj_eval.lamb(1); 
+                traj_eval.phi(2); 
+                traj_eval.eps(2); 
+                traj_eval.lamb(2);
+            ];
             x_diff = x - x_d;
             
             tau = te - t;
@@ -57,8 +70,7 @@ classdef TimeVariantLQR < HeliController
         end
         
         function solve_riccati(obj)
-            obj.riccati_tau = 0:obj.riccati_step_width:obj.ref_t(end);
-            tau_e = obj.ref_t(end);
+            tau_e = obj.trajectory.t(end);
             P_triu_0 = TimeVariantLQR.full_to_triu(obj.S);
             
             [ode_tau, P_triu] = ode45(@(tau, P_triu) TimeVariantLQR.riccati_rhs(obj, tau, P_triu), [0, tau_e], P_triu_0);
@@ -67,25 +79,17 @@ classdef TimeVariantLQR < HeliController
         end
         
         function [A, B] = get_SS_at_time(obj, t)
-            u = interp1(obj.ref_t, obj.ref_u, t);
-            if t > obj.ref_t(end)
-                u = obj.ref_u(end,:);
-            end
+            traj_eval = obj.trajectory.eval(t);
             
-            x = interp1(obj.ref_t, obj.ref_x, t);
-            if t > obj.ref_t(end)
-                x = obj.ref_x(end,:);
-            end
+            x1 = traj_eval.phi(1);
+            x2 = traj_eval.eps(1);
+            x3 = traj_eval.lamb(1);
+            x4 = traj_eval.phi(2);
+            x5 = traj_eval.eps(2);
+            x6 = traj_eval.lamb(2);
             
-            x1 = x(1);
-            x2 = x(2);
-            x3 = x(3);
-            x4 = x(4);
-            x5 = x(5);
-            x6 = x(6);
-            
-            u1 = u(1);
-            u2 = u(2);
+            u1 = traj_eval.vf(1);
+            u2 = traj_eval.vb(1);
             
             A = compute_A(x1, x2, x3, x4, x5, x6, u1, u2);
             B = compute_B(x1, x2, x3, x4, x5, x6, u1, u2);
@@ -109,7 +113,7 @@ classdef TimeVariantLQR < HeliController
         end
         
         function d_P_triu = riccati_rhs(obj, tau, P_triu)
-            te = obj.ref_t(end);
+            te = obj.trajectory.t(end);
             [A, B] = obj.get_SS_at_time(te - tau);
             P = TimeVariantLQR.triu_to_full(P_triu);
             
