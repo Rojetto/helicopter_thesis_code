2393a66b3cd8ed8789846e9bb23e56fe0a9a37dc
Robert Heedt <robert.heedt@gmail.com>
2019-05-28 15:06:11 +0200

    Add lower and upper bound model constants

--- ModelConstants.py
+++ ModelConstants.py
@@ -68,3 +68,61 @@ class RandomizedConstants:
     K_m = OriginalConstants.K_m * random.uniform(0.9, 1.1)
 
     g = OriginalConstants.g
+
+
+class LowerBoundConstants:
+    # l_a = ? (not given in Fig. 7)
+    l_p = OriginalConstants.l_p * 0.9
+    l_h = OriginalConstants.l_h * 0.9
+    l_c = OriginalConstants.l_c * 0.9
+    m_p = OriginalConstants.m_p * 0.9
+    m_c_max = 2 * l_h / l_c * m_p
+    m_c = m_c_max*0.7  # arbitrary, but has to be smaller than m_c_max
+
+    # motor characteristics
+    m_m = 0.2 * m_p
+    r_m = l_p / 8
+    J_m = 0.5 * m_m * r_m ** 2
+
+    d_p = OriginalConstants.d_p * 0.9
+    d_e = OriginalConstants.d_e * 0.9
+    d_l = OriginalConstants.d_l * 0.9
+
+    # PT1 charakteristics for ROTOR SPEED
+    T_f = OriginalConstants.T_f * 0.9
+    T_b = OriginalConstants.T_b * 0.9
+    K_f = OriginalConstants.K_f * 0.9
+    K_b = OriginalConstants.K_b * 0.9
+    K = OriginalConstants.K * 0.9
+    K_m = OriginalConstants.K_m * 0.9
+
+    g = OriginalConstants.g
+
+
+class UpperBoundConstants:
+    # l_a = ? (not given in Fig. 7)
+    l_p = OriginalConstants.l_p * 1.1
+    l_h = OriginalConstants.l_h * 1.1
+    l_c = OriginalConstants.l_c * 1.1
+    m_p = OriginalConstants.m_p * 1.1
+    m_c_max = 2 * l_h / l_c * m_p
+    m_c = m_c_max*0.9  # arbitrary, but has to be smaller than m_c_max
+
+    # motor characteristics
+    m_m = 0.2 * m_p
+    r_m = l_p / 8
+    J_m = 0.5 * m_m * r_m ** 2
+
+    d_p = OriginalConstants.d_p * 1.1
+    d_e = OriginalConstants.d_e * 1.1
+    d_l = OriginalConstants.d_l * 1.1
+
+    # PT1 charakteristics for ROTOR SPEED
+    T_f = OriginalConstants.T_f * 1.1
+    T_b = OriginalConstants.T_b * 1.1
+    K_f = OriginalConstants.K_f * 1.1
+    K_b = OriginalConstants.K_b * 1.1
+    K = OriginalConstants.K * 1.1
+    K_m = OriginalConstants.K_m * 1.1
+
+    g = OriginalConstants.g
