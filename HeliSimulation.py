Fix wrong sign in Fr_inv

HeliSimulation.py
@@ -58,7 +58,7 @@ def Fr_inverse(F):
     expr_1 = (F - mc.q2) / mc.p2
     cond_2 = F < 0
     if isinstance(F, sp.Expr) or F <= 0:  # odd workaround to keep np.sqrt from throwing errors
-        expr_2 = sqrt(-4*mc.q2*F) / mc.p2
+        expr_2 = - sqrt(-4*mc.q2*F) / mc.p2
     else:
         expr_2 = 0
     cond_3 = F < mc.q1