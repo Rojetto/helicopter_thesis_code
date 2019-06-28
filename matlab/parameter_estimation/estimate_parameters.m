e63ccdc60db5aa1c884f6c8cca76fb5f54edd17e
Robert Heedt <robert.heedt@gmail.com>
2019-06-28 13:53:40 +0200

    Something

--- matlab/parameter_estimation/estimate_parameters.m
+++ matlab/parameter_estimation/estimate_parameters.m
@@ -8,7 +8,7 @@ lh = 0.67;
 
 h = 0.002;
 
-load sine_travel
+load friction_pitch_strings
 
 t = log.time;
 Ff = min(Fr(log.signals(1).values, p1, q1, p2, q2), 5);
@@ -51,6 +51,17 @@ plot(t, ddeps)
 
 n = numel(t);
 
+%% only phi
+A = zeros(n, 2);
+b = zeros(n, 1);
+A(:,1) = ddphi;
+A(:,2) = dphi;
+
+b(:) = lp.*(Ff-Fb);
+
+p = (A'*A) \ (A'*b)
+
+%% phi and eps simultaneously
 A = zeros(2*n, 6);
 b = zeros(2*n, 1);
 
