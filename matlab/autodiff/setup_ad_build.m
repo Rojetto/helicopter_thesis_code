e3dc5da765ed86de2d1ed066950a80ac8ecef978
Robert Heedt <robert.heedt@gmail.com>
2019-07-23 18:01:01 +0200

    Remove is_setup from ad build setup

--- matlab/autodiff/setup_ad_build.m
+++ matlab/autodiff/setup_ad_build.m
@@ -1,18 +1,8 @@
 function setup_ad_build()
-persistent is_setup
+base_dir = 'C:\Users\robertph\Desktop\HeliControl\matlab\autodiff\';
 
-if isempty(is_setup)
-    is_setup = false;
-end
-
-if ~is_setup
-    base_dir = 'C:\Users\robertph\Desktop\HeliControl\matlab\autodiff\';
-    
-    coder.updateBuildInfo('addIncludePaths', [base_dir 'include'])
-    coder.updateBuildInfo('addLinkObjects', 'heli_adolc.lib', [base_dir 'lib'], '', true, true)
-    coder.updateBuildInfo('addLinkObjects', 'adolc.lib', [base_dir 'lib'], '', true, true)
-    
-    is_setup = true;
-end
+coder.updateBuildInfo('addIncludePaths', [base_dir 'include'])
+coder.updateBuildInfo('addLinkObjects', 'heli_adolc.lib', [base_dir 'lib'], '', true, true)
+coder.updateBuildInfo('addLinkObjects', 'adolc.lib', [base_dir 'lib'], '', true, true)
 end
 
