50c98a378d2c90a91ee214822696b650c11509bd
Robert Heedt <robert.heedt@gmail.com>
2019-08-07 14:34:25 +0200

    Add failsafe to phi_inv loop

--- matlab/util/phi_inv.m
+++ matlab/util/phi_inv.m
@@ -1,5 +1,6 @@
 function x = phi_inv(z, x_guess)
 step_tol = 1e-9;
+step_limit = 100;
 
 if nargin==1
     x_guess = [0; 0; -0.2; 0.02; 0; 0; 0; 0];
@@ -7,6 +8,7 @@ end
 
 x = x_guess;
 
+i = 0;
 while true
     if coder.target('MATLAB')
         phi = c_calcPhi_mex(x);
@@ -19,8 +21,14 @@ while true
     dx = phi_jac \ (z - phi);
     
     x = x + dx;
+    i = i + 1;
     
     if max(abs(dx)) < step_tol
+        break
+    end
+    
+    if i >= step_limit
+        disp('phi_inv did not converge, aborting')
         break
     end
 end
